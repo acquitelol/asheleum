4835d0f0743ba62b9c45584a0bc489fa84f10f01
Rosie <acquitefx@gmail.com>
2026-09-07 15:56:42 +0100

    feat: loading when adding album to denote activity

--- client/src/components/albums/AlbumInput.tsx
+++ client/src/components/albums/AlbumInput.tsx
@@ -4,8 +4,10 @@ import { useState } from "react";
 import Button from "@/components/Button";
 import { addAlbum } from "@/lib/albums";
 import AlbumAddIcon from "../icons/AlbumAddIcon";
+import LoadingIcon from "../icons/LoadingIcon";
 
 export default function AlbumInput() {
+  const [loading, setLoading] = useState(false);
   const [albumUrl, setAlbumUrl] = useState("");
   const { setAlbums } = useAlbums();
 
@@ -14,7 +16,9 @@ export default function AlbumInput() {
       <h3>Add a new album:</h3>
       <form
         className={styles.inputForm}
-        onSubmit={(e) => addAlbum(e, albumUrl, setAlbumUrl, setAlbums)}
+        onSubmit={(e) =>
+          addAlbum(e, albumUrl, setLoading, setAlbumUrl, setAlbums)
+        }
       >
         <input
           className={styles.textInput}
@@ -22,10 +26,32 @@ export default function AlbumInput() {
           value={albumUrl}
           onChange={(e) => setAlbumUrl(e.target.value)}
           placeholder="Album URL"
+          style={{
+            pointerEvents: loading ? "none" : "auto",
+            opacity: loading ? 0.5 : 1,
+          }}
+          disabled={loading}
         />
-        <Button type="submit" border className={styles.button}>
-          <AlbumAddIcon size={18} />
-          Add
+        <Button
+          type="submit"
+          border
+          className={styles.button}
+          style={{
+            pointerEvents: loading ? "none" : "auto",
+            opacity: loading ? 0.5 : 1,
+          }}
+        >
+          {loading ? (
+            <>
+              <LoadingIcon size={18} />
+              Loading
+            </>
+          ) : (
+            <>
+              <AlbumAddIcon size={18} />
+              Add
+            </>
+          )}
         </Button>
       </form>
     </>
