d06b8202d0f54b80268e0559b1aa46ca13b4bcd0
Rosie <acquitefx@gmail.com>
2026-09-07 09:41:14 +0100

    fix: use better auth url

--- client/src/lib/auth.ts
+++ client/src/lib/auth.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react";
-import { API_URL, BASE_URL } from "@/lib/constants";
+import { API_URL } from "@/lib/constants";
 
 export const authClient = createAuthClient({
   baseURL: API_URL,
@@ -8,13 +8,13 @@ export const authClient = createAuthClient({
 export async function signIn() {
   await authClient.signIn.social({
     provider: "google",
-    callbackURL: BASE_URL,
+    callbackURL: window.location.origin,
   });
 }
 
 export async function signOut() {
   await authClient.signOut({
-    callbackURL: BASE_URL,
+    callbackURL: window.location.origin,
   });
 
   location.reload();
--- server/src/auth.ts
+++ server/src/auth.ts
@@ -4,6 +4,8 @@ import { db } from "./db/index.js";
 import * as schema from "./schema.js";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL!,
+
   database: drizzleAdapter(db, {
     provider: "pg",
     schema,
